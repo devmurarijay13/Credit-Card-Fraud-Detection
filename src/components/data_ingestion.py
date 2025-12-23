76ccec1aafe25946eca78bc4b2801d8f87ea7911
devmurarijay13 <devmurarijay66@gmail.com>
2025-12-23 22:26:23 +0530

    Data Ingestion Completed

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,51 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import StratifiedKFold,train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str = os.path.join('artifacts','train.csv')
+    test_data_path:str = os.path.join('artifacts','test.csv')
+    raw_data_path:str = os.path.join('artifacts','data.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        try:
+            df = pd.read_csv('notebook/data/creditcard.csv')
+            logging.info('read the data as dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info('spliting train and test data')
+
+            train_set,test_set = train_test_split(
+                df,
+                random_state=42,
+                test_size=0.2,
+                stratify=df["Class"]
+                )
+            
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info("completed the data ingestion")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+
+        
+if __name__=="__main__":
+    di_obj = DataIngestion()
+    di_obj.initiate_data_ingestion()
