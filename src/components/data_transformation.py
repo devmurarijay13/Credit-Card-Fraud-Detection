9ad2fc37b7b14b2bcc34a15d2cb3f26f02fe6972
devmurarijay13 <devmurarijay66@gmail.com>
2025-12-24 17:15:08 +0530

    Data Transformation Completed

---
+++ src/components/data_transformation.py
@@ -0,0 +1,80 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+from dataclasses import dataclass
+from sklearn.preprocessing import RobustScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join('artifacts','preprocessor.pkl')
+
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformer_obj(self):
+        try:
+            numerical_features = ['Time','Amount']
+
+            preprocessor = ColumnTransformer(
+                transformers=[
+                    ('scaler',RobustScaler(),numerical_features)
+                ],
+                remainder='passthrough'
+            )
+
+            return preprocessor
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    def initiate_data_transformation(self,train_path,test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info("read the train and test as df")
+
+            target_col = 'Class'
+
+            X_train = train_df.drop(columns=[target_col])
+            y_train = train_df[target_col]
+
+            X_test = test_df.drop(columns=[target_col])
+            y_test = test_df[target_col]
+
+            preprocessor = self.get_data_transformer_obj()
+
+            X_train_trf = preprocessor.fit_transform(X_train)
+            X_test_trf = preprocessor.transform(X_test)
+            logging.info('preprocessing applied')
+
+            train_arr = np.c_[
+                X_train_trf,np.array(y_train)
+            ]
+
+            test_arr = np.c_[
+                X_test_trf,np.array(y_test)
+            ]
+
+            save_object(
+                file_path = self.data_transformation_config.preprocessor_obj_file_path,
+                obj = preprocessor
+            )
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
