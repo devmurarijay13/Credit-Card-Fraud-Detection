c072c17bd77f44b2e075f8649519b7c1a53ca6e5
devmurarijay13 <devmurarijay66@gmail.com>
2025-12-25 20:36:16 +0530

    update the app.py

--- app.py
+++ app.py
@@ -1,41 +1,53 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
-import joblib
+import pickle
 import numpy as np
 import pandas as pd
+from src.logger import logging
 
 MODEL_PATH = "artifacts/model.pkl"
 PREPROCESSOR_PATH = "artifacts/preprocessor.pkl"
 FEATURES_PATH = "artifacts/features.pkl"
 
-model = joblib.load(MODEL_PATH)
-preprocessor = joblib.load(PREPROCESSOR_PATH)
-features = joblib.load(FEATURES_PATH)
+with open(MODEL_PATH, "rb") as f:
+    model = pickle.load(f)
+
+with open(PREPROCESSOR_PATH, "rb") as f:
+    preprocessor = pickle.load(f)
+
+with open(FEATURES_PATH, "rb") as f:
+    features = pickle.load(f)
 
 app = FastAPI(title="Credit Card Fraud Detection API")
 
-
 class Transaction(BaseModel):
     data: dict   # { "V1": value, "V2": value, ..., "Amount": value }
-
 
 @app.get("/")
 def home():
     return {"status": "API is running"}
 
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
 ##prediction endpoint
 @app.post("/predict")
 def predict_fraud(transaction: Transaction):
+    try:
+        df = pd.DataFrame([transaction.data])
+        X = preprocessor.transform(df)
 
-    df = pd.DataFrame([transaction.data])
-    X = preprocessor.transform(df)
+        prob = model.predict_proba(X)[:, 1][0]
 
-    prob = model.predict_proba(X)[:, 1][0]
+        threshold = 0.7
+        prediction = int(prob >= threshold)
 
-    threshold = 0.7
-    prediction = int(prob >= threshold)
+        logging.info(f"Prediction made: {prediction[0]}")
+        return {
+            "fraud_probability": round(float(prob), 4),
+            "is_fraud": prediction
+        }
 
-    return {
-        "fraud_probability": round(float(prob), 4),
-        "is_fraud": prediction
-    }
+    except Exception as e:
+        raise HTTPException(status_code=400,detail=str(e))
